9aba5020260d87018adc09afeeff248bd7c718e3
David Pechtalt <davidpechtalt@gmail.com>
2024-12-23 13:31:38 +0200

    moving to index

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -18,12 +18,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body className="flex flex-col">
-        <div className=" bg-purple-900 h-10 "></div>
-        <div className="flex w-[100vw] h-[calc(100vh-40px)] bg-purple-900 overflow-hidden ">
-          <div className="w-[70px] min-w-12  max-[895px]:w-12"></div>
-          <div className="min-w-44 bg-purple-800 h-100% rounded-l-md flex-grow-[0.24]"></div>
-          <div className="bg-white mr-1 mb-1 rounded-r-md flex-grow-[0.76]"></div>
-        </div>
+  
         {children}
         <ScrollRestoration />
         <Scripts />
--- my-remix-app/app/routes/_index.tsx
+++ my-remix-app/app/routes/_index.tsx
@@ -9,8 +9,14 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-  <div ></div>
+    <div className="flex flex-col">
+      {" "}
+      <div className=" bg-purple-900 h-10 "></div>
+      <div className="flex w-[100vw] h-[calc(100vh-40px)] bg-purple-900 overflow-hidden ">
+        <div className="w-[70px] min-w-12  max-[895px]:w-12"></div>
+        <div className="min-w-44 bg-purple-800 h-100% rounded-l-md flex-grow-[0.24]"></div>
+        <div className="bg-white mr-1 mb-1 rounded-r-md flex-grow-[0.76]"></div>
+      </div>
+    </div>
   );
 }
-
-
