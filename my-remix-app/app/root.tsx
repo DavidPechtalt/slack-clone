4a19919a46ca784e5c8b6dc50a8a079e6215ce1a
David Pechtalt <davidpechtalt@gmail.com>
2024-12-23 13:19:59 +0200

    perfect with slack

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -8,8 +8,6 @@ import {
 
 import "./tailwind.css";
 
-
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -21,11 +19,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body className="flex flex-col">
         <div className=" bg-purple-900 h-10 "></div>
-        <div className="flex w-[100vw] h-[calc(100vh-40px)] bg-purple-900 ">
-          <div className="w-[70px] min-w-12"></div>
-          <div className="min-w-44 bg-purple-800 h-100% rounded-l-md flex-grow-[0.24]"  ></div>
-          <div className="bg-white mr-1 mb-1 rounded-r-md flex-grow-1 flex-grow-[0.76]">
-          </div>
+        <div className="flex w-[100vw] h-[calc(100vh-40px)] bg-purple-900 overflow-hidden ">
+          <div className="w-[70px] min-w-12  max-[895px]:w-12"></div>
+          <div className="min-w-44 bg-purple-800 h-100% rounded-l-md flex-grow-[0.24]"></div>
+          <div className="bg-white mr-1 mb-1 rounded-r-md flex-grow-[0.76]"></div>
         </div>
         {children}
         <ScrollRestoration />
